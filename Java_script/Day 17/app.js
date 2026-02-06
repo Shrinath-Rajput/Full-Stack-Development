9ee8ff0408e310fb4cbfd079c454dd79711c3dc8
Shrinath Rajput <rajputshrinath349@gmail.com>
2026-02-06 20:08:49 +0530

    al

---
+++ Java_script/Day 17/app.js
@@ -0,0 +1,51 @@
+// for(let i=1;i<=7;i++)
+// {
+//     console.log(i)
+// }
+
+// print odd number
+
+// for (let i=1;i<=15;i=i+2)
+// {
+//     console.log(i)
+// }
+
+// Even Number
+
+// for (let i=0;i<=12;i=i+2)
+// {
+//     console.log(i)
+// }
+
+// table
+
+// let k=prompt('Enter your no')
+// for (let i=1;i<=10;i++)
+// {
+//     console.log(k*i)
+// }
+
+// let i=1;
+// while( i<=15)
+// {
+//     console.log(i)
+//     i=i+2
+    
+// }
+
+// let fav_cricketer="Virat";
+// guess1=prompt("Guess your fave cricketer")
+// while(fav_cricketer!=guess1)
+// {
+//     guess1=prompt("try again")
+// }
+// if (fav_cricketer==guess1)
+// {
+//     console.log("Congrates_____")
+// }
+
+let list1=['apple',"mango",'pineaplle','apple','banana','orange'];
+for (i=0;i<list1.length;i++)
+{
+    console.log(i,list1[i])
+}
---
+++ Java_script/Day 17/repet.py
@@ -0,0 +1,56 @@
+# list=[]
+# for i in range(0,7):
+#     print(i)
+
+#     list.append(i)
+#     print(list)
+
+
+
+#print odd numbers
+
+# for i in range(16):
+#     if i%2==1:
+#         print(i)
+
+#Even number
+
+# for i in range(12):
+#     if i %2==0:
+#         print(i)
+
+#table
+# a=int(input("Enter your no"))
+# def table(a):
+#     for i in range(1,11):
+#         print(a,"*",i,"=",a*i)
+# table(a)
+
+# count=1
+# while count<=10:
+#    i=count+2
+#    print(i)
+   
+# i=0   
+# list1=['apple',"mango",'pineaplle','apple','banana','orange']
+# while i <=len(list1):
+    
+#     print(list1[i])
+#     i+=1
+    
+i=0
+dict1={
+    "name":"shrinath",
+    "surname":"rajput",
+    "favcri":"virat",
+    "marks":
+    {
+        "english":88,
+        "python":98,
+        "Ml":97
+    }
+}
+while i<=len(dict1):
+    
+        print(dict1(i))
+        i+=1
