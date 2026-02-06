d187e4b34d0c5aebe7b95e2299d6fa6d0acddb8b
Shrinath Rajput <rajputshrinath349@gmail.com>
2026-02-06 11:25:28 +0530

    all

---
+++ Java_script/Day 15/app.js
@@ -0,0 +1,60 @@
+// console.log('I Am Shrinath Rajput')
+
+// let penchil=10;
+// let scale=10;
+// let output1=`The penchil price is ${penchil} rs .`
+// console.log(output1)
+// let price="Total is"+" "+(penchil+scale)+"rs ."
+// console.log(price)
+
+//   let num=19;
+//   if (num %2==0)
+//   {
+//      console.log("Even Number")
+//   }
+//    else {
+//     console.log("Odd")
+//   }
+ 
+
+
+
+// let Red;
+// let Yellow;
+// let Green;
+
+// if (Red==0)
+// {
+//     console.log("Stop ")
+// }
+
+// if (Yellow==0);
+// {
+//     console.log("keep going on Slow")
+// }
+
+// if (Green==0)
+// {
+//     console.log("Go Early")
+// }
+
+
+let marks=prompt("Enter your marks")
+if (marks>=33)
+    if(marks==33)
+    {
+        console.log("Good")
+    }
+    else{
+        console.log("Not Good")
+    }
+console.log(marks)
+
+// let str='apple';
+// if (str[0]=='a' && str>[3] )
+    
+// {
+//     console.log("GOOD")
+// }
+
+alert("Spmething is Wrong")
