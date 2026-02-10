150ec8d4ec4c3b2bc4ddaaed48463747d8c704f5
Shrinath Rajput <rajputshrinath349@gmail.com>
2026-02-10 19:45:25 +0530

    all

---
+++ Java_script/Day 20/app.js
@@ -0,0 +1,40 @@
+// console.log("Hello");
+// console.log("Shrinath");
+
+// try{
+//     console.log(a);
+// }
+// catch{
+//     console.log("A is not defined ctch that error ");
+// }
+// console.log("Hello KK")
+
+// let sum=()=>
+// {
+//     let a=Number(prompt("Enter your first no"));
+//     let b=Number(prompt("Enter your second no"));
+//     // console.log(a)
+//     // console.log(b)
+//     console.log(a + b);
+// }
+// sum()
+
+// let cube=()=>
+// {
+//     let n=Number(prompt("Enter your n"));
+//     return n*n*n;
+// }
+// console.log(cube());
+
+
+let time=setTimeout(() => {
+    console.log("Yes , I Am Shrinath Rajput")
+  
+    
+}, 5000);
+
+let time1=setInterval(() => {
+    console.log("Yes , I Am Shrinath Rajput")
+  
+    
+}, 5000);
