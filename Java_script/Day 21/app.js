8f59913c29942840d172df6f629f51b9b4987e83
Shrinath Rajput <rajputshrinath349@gmail.com>
2026-02-11 23:04:26 +0530

    all

---
+++ Java_script/Day 21/app.js
@@ -0,0 +1,15 @@
+// let print=[1,2,3,4,5,'shri','virat'];
+
+// let arr=(el)=>
+// {
+//     console.log(el);
+// }
+// print.forEach(arr);
+
+
+let shri=[1,2,3,4];
+let double=(el)=>
+{
+    console.log(el*2);
+}
+shri.forEach(double);
