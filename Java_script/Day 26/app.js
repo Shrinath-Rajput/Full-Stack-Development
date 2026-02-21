7f37c9b1ebc71002d9d74573585e8cd54cd6f74b
Shrinath Rajput <rajputshrinath349@gmail.com>
2026-02-21 20:23:28 +0530

    all

--- Java_script/Day 26/app.js
+++ Java_script/Day 26/app.js
@@ -66,25 +66,67 @@
 // }
 
 
-let h1 = document.querySelector("h1");
-h1.style.color = "blue";
+// let h1 = document.querySelector("h1");
+// h1.style.color = "blue";
 
 
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+// function delay(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
-async function changeColor() {
-    let h1 = document.querySelector("h1");
+// async function changeColor() {
+//     let h1 = document.querySelector("h1");
 
-    await delay(1000);
-    h1.style.color = "red";
+//     await delay(1000);
+//     h1.style.color = "red";
 
-    await delay(2000);
-    h1.style.color = "green";
+//     await delay(2000);
+//     h1.style.color = "green";
 
-    await delay(3000);
-    h1.style.color = "blue";
-}
+//     await delay(3000);
+//     h1.style.color = "blue";
+// }
 
-changeColor();
+// changeColor();
+
+// // jason file
+// // 1 .parse method
+// jsonres=`{"fact":"The Pilgrims were the first to introduce cats to North America.","length":63}`
+
+// let result=JSON.parse(jsonres);
+// console.log(result);
+
+// // 2.stringify method
+// let obj={
+//     name:"Shrinath",
+//     surname:"rajput",
+//     marks:99,
+//     sub:{
+//         firs:"Maths",
+//         second:"ENglish",
+//         Third:"Geo",
+
+//     },
+//     all:"Good "
+// }
+// console.log(obj);
+// let res=JSON.stringify(obj);
+// console.log(res);
+
+
+// our first request using fetch
+
+let url="https://catfact.ninja/fact";
+fetch(url)
+.then((res)=>
+{
+    console.log(`Response is ${res}`);
+    console.log(res.json().then((data)=>
+    {
+        console.log(data);
+    }))
+})
+.catch((error)=>
+{
+console.log(`Error is ${error}`);
+})
